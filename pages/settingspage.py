66a07ed5d775b055c273aadd8d820ba3eb6e6b17
fulltilt <fulltilt@fulltilts-MacBook-Pro.local>
2020-10-02 01:16:16 -0700

    Added all files with formatting

--- pages/settingspage.py
+++ pages/settingspage.py
@@ -4,9 +4,13 @@ import sys
 import platform
 import settings
 
+
 def no_abort(a, b, c):
     sys.__excepthook__(a, b, c)
+
+
 sys.excepthook = no_abort
+
 
 class SettingsPage(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -18,50 +22,56 @@ class SettingsPage(QtWidgets.QWidget):
         self.settingspage.setAttribute(QtCore.Qt.WA_StyledBackground, True)
         self.settingspage.setGeometry(QtCore.QRect(60, 0, 1041, 601))
         self.settingspage.setStyleSheet(
-            "QComboBox::drop-down {    border: 0px;}QComboBox::down-arrow {    image: url(:/images/down_icon.png);    width: 14px;    height: 14px;}QComboBox{    padding: 1px 0px 1px 3px;}QLineEdit:focus {   border: none;   outline: none;}") # don't know if this line is needed
+            "QComboBox::drop-down {    border: 0px;}QComboBox::down-arrow {    image: url(:/images/down_icon.png);    width: 14px;    height: 14px;}QComboBox{    padding: 1px 0px 1px 3px;}QLineEdit:focus {   border: none;   outline: none;}"
+        )  # don't know if this line is needed
 
         # Settings header
         self.proxies_header = QtWidgets.QLabel(self.settingspage)
         self.proxies_header.setGeometry(QtCore.QRect(30, 10, 81, 31))
         font = QtGui.QFont()
         font.setFamily("Arial")
-        font.setPointSize(22) if platform.system(
-        ) == "Darwin" else font.setPointSize(22*.75)
+        font.setPointSize(22) if platform.system() == "Darwin" else font.setPointSize(
+            22 * 0.75
+        )
         font.setWeight(50)
         self.proxies_header.setFont(font)
         self.proxies_header.setStyleSheet("color: rgb(234, 239, 239);")
         self.proxies_header.setText("Settings")
-        
+
         self.settings_card = QtWidgets.QWidget(self.settingspage)
         self.settings_card.setGeometry(QtCore.QRect(30, 70, 471, 501))
         font = QtGui.QFont()
-        font.setPointSize(13) if platform.system(
-        ) == "Darwin" else font.setPointSize(13*.75)
+        font.setPointSize(13) if platform.system() == "Darwin" else font.setPointSize(
+            13 * 0.75
+        )
         font.setFamily("Arial")
         self.settings_card.setFont(font)
         self.settings_card.setStyleSheet(
-            "background-color: #232323;border-radius: 20px;border: 1px solid #2e2d2d;")
-        
+            "background-color: #232323;border-radius: 20px;border: 1px solid #2e2d2d;"
+        )
+
         self.webhook_header = QtWidgets.QLabel(self.settings_card)
         self.webhook_header.setGeometry(QtCore.QRect(20, 10, 101, 31))
         font = QtGui.QFont()
         font.setFamily("Arial")
-        font.setPointSize(18) if platform.system(
-        ) == "Darwin" else font.setPointSize(18*.75)
+        font.setPointSize(18) if platform.system() == "Darwin" else font.setPointSize(
+            18 * 0.75
+        )
         font.setWeight(50)
         self.webhook_header.setFont(font)
-        self.webhook_header.setStyleSheet(
-            "color: rgb(212, 214, 214);border:  none;")
+        self.webhook_header.setStyleSheet("color: rgb(212, 214, 214);border:  none;")
         self.webhook_header.setText("Webhook")
 
         # setup input elements
-        font.setPointSize(13) if platform.system(
-        ) == "Darwin" else font.setPointSize(18*.75)
+        font.setPointSize(13) if platform.system() == "Darwin" else font.setPointSize(
+            18 * 0.75
+        )
         self.webhook_edit = QtWidgets.QLineEdit(self.settings_card)
         self.webhook_edit.setGeometry(QtCore.QRect(30, 50, 411, 21))
         self.webhook_edit.setFont(font)
         self.webhook_edit.setStyleSheet(
-            "outline: 0;border: 1px solid #5D43FB;border-width: 0 0 2px;color: rgb(234, 239, 239);")
+            "outline: 0;border: 1px solid #5D43FB;border-width: 0 0 2px;color: rgb(234, 239, 239);"
+        )
         self.webhook_edit.setPlaceholderText("Webhook Link")
         self.webhook_edit.setAttribute(QtCore.Qt.WA_MacShowFocusRect, 0)
         self.browser_checkbox = QtWidgets.QCheckBox(self.settings_card)
@@ -74,20 +84,19 @@ class SettingsPage(QtWidgets.QWidget):
         self.order_checkbox.setText("Order Placed")
         self.paymentfailed_checkbox = QtWidgets.QCheckBox(self.settings_card)
         self.paymentfailed_checkbox.setGeometry(QtCore.QRect(30, 150, 121, 20))
-        self.paymentfailed_checkbox.setStyleSheet(
-            "color: #FFFFFF;border: none;")
+        self.paymentfailed_checkbox.setStyleSheet("color: #FFFFFF;border: none;")
         self.paymentfailed_checkbox.setText("Payment Failed")
 
         self.general_header = QtWidgets.QLabel(self.settings_card)
         self.general_header.setGeometry(QtCore.QRect(20, 180, 101, 31))
         font = QtGui.QFont()
         font.setFamily("Arial")
-        font.setPointSize(18) if platform.system(
-        ) == "Darwin" else font.setPointSize(18*.75)
+        font.setPointSize(18) if platform.system() == "Darwin" else font.setPointSize(
+            18 * 0.75
+        )
         font.setWeight(50)
         self.general_header.setFont(font)
-        self.general_header.setStyleSheet(
-            "color: rgb(212, 214, 214);border:  none;")
+        self.general_header.setStyleSheet("color: rgb(212, 214, 214);border:  none;")
         self.general_header.setText("General")
         self.onfailed_checkbox = QtWidgets.QCheckBox(self.settings_card)
         self.onfailed_checkbox.setGeometry(QtCore.QRect(30, 220, 221, 20))
@@ -101,14 +110,15 @@ class SettingsPage(QtWidgets.QWidget):
         self.savesettings_btn = QtWidgets.QPushButton(self.settings_card)
         self.savesettings_btn.setGeometry(QtCore.QRect(190, 450, 86, 32))
         font = QtGui.QFont()
-        font.setPointSize(13) if platform.system(
-        ) == "Darwin" else font.setPointSize(13*.75)
+        font.setPointSize(13) if platform.system() == "Darwin" else font.setPointSize(
+            13 * 0.75
+        )
         font.setFamily("Arial")
         self.savesettings_btn.setFont(font)
-        self.savesettings_btn.setCursor(
-            QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.savesettings_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.savesettings_btn.setStyleSheet(
-            "color: #FFFFFF;background-color: #5D43FB;border-radius: 10px;border: 1px solid #2e2d2d;")
+            "color: #FFFFFF;background-color: #5D43FB;border-radius: 10px;border: 1px solid #2e2d2d;"
+        )
         self.savesettings_btn.setText("Save")
         self.savesettings_btn.clicked.connect(self.save_settings)
 
@@ -127,7 +137,7 @@ class SettingsPage(QtWidgets.QWidget):
             self.paymentfailed_checkbox.setChecked(True)
         if settings["browseronfailed"]:
             self.onfailed_checkbox.setChecked(True)
-        if settings['onlybuyone']:
+        if settings["onlybuyone"]:
             self.buy_one_checkbox.setChecked(True)
         self.update_settings(settings)
 
@@ -138,7 +148,7 @@ class SettingsPage(QtWidgets.QWidget):
             "webhookonorder": self.order_checkbox.isChecked(),
             "webhookonfailed": self.paymentfailed_checkbox.isChecked(),
             "browseronfailed": self.onfailed_checkbox.isChecked(),
-            "onlybuyone": self.buy_one_checkbox.isChecked()
+            "onlybuyone": self.buy_one_checkbox.isChecked(),
         }
         write_data("./data/settings.json", settings)
         self.update_settings(settings)
@@ -146,5 +156,18 @@ class SettingsPage(QtWidgets.QWidget):
 
     def update_settings(self, settings_data):
         global webhook, webhook_on_browser, webhook_on_order, webhook_on_failed, browser_on_failed
-        settings.webhook, settings.webhook_on_browser, settings.webhook_on_order, settings.webhook_on_failed, settings.browser_on_failed, settings.buy_one = settings_data[
-            "webhook"], settings_data["webhookonbrowser"], settings_data["webhookonorder"], settings_data["webhookonfailed"], settings_data["browseronfailed"], settings_data['onlybuyone']
+        (
+            settings.webhook,
+            settings.webhook_on_browser,
+            settings.webhook_on_order,
+            settings.webhook_on_failed,
+            settings.browser_on_failed,
+            settings.buy_one,
+        ) = (
+            settings_data["webhook"],
+            settings_data["webhookonbrowser"],
+            settings_data["webhookonorder"],
+            settings_data["webhookonfailed"],
+            settings_data["browseronfailed"],
+            settings_data["onlybuyone"],
+        )
